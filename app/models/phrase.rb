ca6f5d678986f5d0f2dd068f1e79f5beddcb8039
Charles Cox <cox.charles.w@gmail.com>
2017-08-22 14:42:47 -0400

    resolve merge

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,13 +1,13 @@
 class Phrase < ActiveRecord::Base
   has_many :photos
 
-  after_create do
-    parsed_phrase = parse(self)
-    photos = query_flickr(parsed_phrase)
-    photos.each do ...
-  end
-
-  private
+  # after_create do
+  #   parsed_phrase = parse(self)
+  #   photos = query_flickr(parsed_phrase)
+  #   photos.each do ...
+  # end
+  #
+  # private
 
   # def parse...
 
