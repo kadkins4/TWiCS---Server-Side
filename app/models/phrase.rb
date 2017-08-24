335cf6bd3eb51022f439cd20963abfe8fbea502e
Kendall <adkins.kendall90@gmail.com>
2017-08-24 13:11:35 -0400

    language parser

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,19 +1,31 @@
 class Phrase < ActiveRecord::Base
   has_many :photos
 
+
   after_create do
-    parsed_phrase = phrase_parse(self)
-    query_flickr(parsed_phrase)
+    tgr = EngTagger.new
+    # Sample text
+    # Add part-of-speech tags to text
+    tagged = tgr.add_tags(phrase.gsub(/(?:f|ht)tps?:\/[^\s]+/, ''))
+    nps = tgr.get_nouns(tagged)
+    new_arr = []
+    for x in nps do
+      new_arr << x[0]
+    end
+    puts "new_arr: #{new_arr}"
+    @phrase_arr = new_arr
+    # parsed_phrase = phrase_parse(self)
+    query_flickr(@phrase_arr)
   end
 
 private
-  def phrase_parse (argument)
-    argument.content.split(' ')
-  end
+  # def phrase_parse (argument)
+  #   argument.content.split(' ')
+  # end
 
   def query_flickr (phrase_arr)
     for x in phrase_arr
-      if x.length > 2 && x != 'the' && x != 'she' && x != 'too' && x != 'this' && x != 'that' && x != 'and'
+      begin
         FlickRaw.api_key=ENV['API_KEY']
         FlickRaw.shared_secret=ENV['SHARED_KEY']
         list = flickr.photos.search tags: x, safe_search: 1
@@ -23,8 +35,7 @@ private
         secret = list.first.secret
         photo_url = "http://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
         Photo.create!(photo_url: photo_url, tags: x, phrase_id: self.id)
-      else
-        next
+      rescue
       end
     end
   end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,34 +2,43 @@ class Tweet < ActiveRecord::Base
   has_many :pictures
 
   before_save do
-    puts 'hello'
-    client = Twitter::REST::Client.new do |config|
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_API_KEY']
       config.consumer_secret     = ENV['TWITTER_API_SECRET']
       config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
-    rtn_tweet = 'x'
-    # need to validate twitter response to make sure we get back a response
-    client.search("from:#{self.handle} -rt -link", result_type: 'recent').take(1).each do |tweet|
-      rtn_tweet = tweet.text
+
+    @handle = self.handle
+
+    def check_user(user)
+      @client.user(user).tweets_count
     end
-    self.content = rtn_tweet
-    puts 'self'
-    puts self
-    puts self.id
-    # need to validate the tweet, removing any uwanted words or symbols
-    tgr = EngTagger.new
-    # Sample text
-    # Add part-of-speech tags to text
-    tagged = tgr.add_tags(rtn_tweet.gsub(/(?:f|ht)tps?:\/[^\s]+/, ''))
-    nps = tgr.get_nouns(tagged)
-    new_arr = []
-    for x in nps do
-      new_arr << x[0]
+
+    begin
+      check_user(@handle)
+    rescue
+      break
+    else
+      rtn_tweet = 'x'
+      # need to validate twitter response to make sure we get back a response
+      client.search("from:#{self.handle} -rt -link", result_type: 'recent').take(1).each do |tweet|
+        rtn_tweet = tweet.text
+      end
+      self.content = rtn_tweet
+      # need to validate the tweet, removing any uwanted words or symbols
+      tgr = EngTagger.new
+      # Sample text
+      # Add part-of-speech tags to text
+      tagged = tgr.add_tags(rtn_tweet.gsub(/(?:f|ht)tps?:\/[^\s]+/, ''))
+      nps = tgr.get_nouns(tagged)
+      new_arr = []
+      for x in nps do
+        new_arr << x[0]
+      end
+      @tweet_arr = new_arr
     end
-    puts "new_arr: #{new_arr}"
-    @tweet_arr = new_arr
+
   end
 
 
@@ -38,17 +47,19 @@ class Tweet < ActiveRecord::Base
   end
 
   def query_flickr (tweet_arr)
-    puts 'flickr'
     for x in tweet_arr
-      FlickRaw.api_key=ENV['API_KEY']
-      FlickRaw.shared_secret=ENV['SHARED_KEY']
-      list = flickr.photos.search tags: x, safe_search: 1
-      farm = list.first.farm
-      server = list.first.server
-      id = list.first.id
-      secret = list.first.secret
-      picture_url = "http://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
-      Picture.create!(picture_url: picture_url, tags: x, tweet_id: self.id)
+      begin
+        FlickRaw.api_key=ENV['API_KEY']
+        FlickRaw.shared_secret=ENV['SHARED_KEY']
+        list = flickr.photos.search tags: x, safe_search: 1
+        farm = list.first.farm
+        server = list.first.server
+        id = list.first.id
+        secret = list.first.secret
+        picture_url = "http://farm#{farm}.staticflickr.com/#{server}/#{id}_#{secret}.jpg"
+        Picture.create!(picture_url: picture_url, tags: x, tweet_id: self.id)
+      rescue
+      end
     end
   end
 end
