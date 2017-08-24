840f3b7f358d8cae36e04772368a9ce64a7ac86f
Kendall <adkins.kendall90@gmail.com>
2017-08-24 13:13:27 -0400

    meh

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,28 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+    render json: @tweets, include: [:pictures]
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    render json: @tweet, include: [:pictures]
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    # @tweet.save!
+    if @tweet.save!
+      render json: @tweet, status: :created, location: @tweet
+    else
+      render json: @tweet, status: :unprocessable_entity
+    end
+  end
+
+private
+  def tweet_params
+    params.require(:tweet).permit(:handle, :content)
+  end
+
+end
